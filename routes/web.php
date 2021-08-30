9945b8900f9b4bb4e8f3b839e056adcb7825a5cf
gitout734 <gitout734@users.noreply.github.com>
2021-08-30 12:54:47 -0500

    adding posts to the homepage

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $posts = \App\Models\Post::all();
+    // dd($posts);
+    return view('welcome', ['posts' => $posts]);
 });
