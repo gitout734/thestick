c9c4aa29c543b4972597d8618db5d4da036704e5
gitout734 <gitout734@users.noreply.github.com>
2021-10-13 16:35:24 -0500

    test hello vs app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 require('./bootstrap');
+// test thunder client
+
+export default function handler(req, res) {
+    res.status(200).json({ name: 'stickman +8'})
+}
--- resources/js/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/development
-
-export default function handler(req, res) {
-    res.status(200).json({ name: 'stickman +8'})
-}
